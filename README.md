19917bd8b608c01327a137c937778df184c23554
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-04-03 13:23:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=𝗪𝗘𝗟𝗖𝗢𝗠+𝗧𝗢+𝐓𝐇𝐄+𝐒𝐍𝐎𝐖𝐁𝐀𝐋𝐋+𝐑𝐄𝐏𝐎!;𝗖𝗥𝗘𝗔𝗧𝗘𝗗+𝗕𝗬+𝗦𝗡𝗢𝗪𝗕𝗔𝗟𝗟!;𝗔+𝗦𝗜𝗠𝗣𝗟𝗘+𝗧𝗚+𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥+𝗕𝗢𝗧!)</p>
+
+# 𝗣𝗢𝗥𝗡 & 𝗠𝗘𝗚𝗔 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥
+
+
+
+
+
+## Deploy To Heroku ⚠️ Deploy On EU Server
+
+<a href="https://heroku.com/deploy?template=https://github.com/SaliraVoda/Pompom"><img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy"></a>
+
+
+## Configs 
+
+* `BOT_TOKEN`  - Get Bot Token From @BotFather
+
+* `API_ID` - From my.telegram.org 
+
+* `API_HASH` - From my.telegram.org
+
+* `WEBHOOK` - If Your Server Is Need Web Service! Value = `True` Else Value = `False`
+
+* `ADMIN` - AUTH Or Bot Controllers Id's Multiple Id Use Space To Split 
+
+* `DB_URL`  - Mongo Database URL From https://cloud.mongodb.com
+
+* `DB_NAME`  - Your Database Name From Mongodb. 
+
+* `FORCE_SUB` - Your Force Sub Channel Chat Id
+
+* `LOG_CHANNEL` - Bot Logs Sending Channel. If You Don't Need This To Remove This Variable In Your Server
+
+* `START_PIC` - Start Message Photo. You Don't Need This! Just Skip
+
+## Botfather Commands
+```
+start - Bot Alive Cheking
+restart - To Rrstart The Bot (Admin Only)
+status - Check Bot Status (Admin Only)
+broadcast - Send Message To All Users (Admin Only)
+```
---
+++ plugins/start.py
@@ -0,0 +1,92 @@
+from pyrogram import Client, filters
+import logging
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from config import Config, Txt
+from .check_user_status import handle_user_status
+from helper.utils import is_subscribed, force_sub
+
+logger = logging.getLogger(__name__)
+
+
+@Client.on_message(filters.private)
+async def _(bot, cmd):
+    if not await is_subscribed(bot, cmd):
+        return await force_sub(bot, cmd)
+    await handle_user_status(bot, cmd)
+    await cmd.continue_propagation()
+
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client: Client, message: Message):
+    user = message.from_user
+    button = InlineKeyboardMarkup([[
+        InlineKeyboardButton(
+            '•Uᴘᴅᴀᴛᴇs•', url='https://t.me/HEROKUBIN_01'),
+        InlineKeyboardButton(
+            '•Sᴜᴘᴘᴏʀᴛ•', url='https://t.me/ll_THE_BAD_BOT_ll')
+    ], [
+        InlineKeyboardButton('•Aʙᴏᴜᴛ•', callback_data='about'),
+        InlineKeyboardButton('•Hᴇʟᴘ•', callback_data='help')
+    ],
+        [
+        InlineKeyboardButton("ᴘᴏʀɴ",
+                             switch_inline_query_current_chat="",)
+    ]])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+
+
+# ⚠️ Handling CallBack Query⚠️
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data
+    if data == "start":
+        await query.message.edit_text(
+            text=Txt.START_TXT.format(query.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(
+                    '• Uᴩᴅᴀᴛᴇꜱ •', url='https://t.me/HEROKUBIN_01'),
+                InlineKeyboardButton(
+                    '• Sᴜᴩᴩᴏʀᴛ •', url='https://t.me/ll_THE_BAD_BOT_ll')
+            ], [
+                InlineKeyboardButton('• Aʙᴏᴜᴛ •', callback_data='about'),
+                InlineKeyboardButton('• Hᴇʟᴩ •', callback_data='help')
+            ],
+                [
+                InlineKeyboardButton("ᴘᴏʀɴ",
+                                     switch_inline_query_current_chat="",)
+            ]])
+        )
+
+    elif data == "help":
+        await query.message.edit_text(
+            text=Txt.HELP_TXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("❌ Cʟᴏꜱᴇ", callback_data="close"),
+                InlineKeyboardButton("❮ Bᴀᴄᴋ", callback_data="start")
+            ]])
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=Txt.ABOUT_TXT.format(client.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("❌ Cʟᴏꜱᴇ", callback_data="close"),
+                InlineKeyboardButton("❮ Bᴀᴄᴋ", callback_data="start")
+            ]])
+        )
+
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+            await query.message.continue_propagation()
+        except:
+            await query.message.delete()
+            await query.message.continue_propagation()
+
